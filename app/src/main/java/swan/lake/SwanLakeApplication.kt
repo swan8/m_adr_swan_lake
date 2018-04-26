4112cd01530ce8a1b9b8f8d00fadfb52864fa17a
stephen.guo <stephen.guo@qunar.com>
2018-04-26 16:32:57 +0800

    use mikepenz draw layout

--- app/src/main/java/swan/lake/SwanLakeApplication.kt
+++ app/src/main/java/swan/lake/SwanLakeApplication.kt
@@ -6,5 +6,6 @@ class SwanLakeApplication : SwanAtomBaseApplication() {
 
     override fun initModuleApplicationImpl(module: MutableList<String>) {
         module.add("swan.atom.core.AtomCoreApplicationImpl")
+        module.add("swan.biz.koala.KoalaApplicationImpl")
     }
 }
--- app/src/main/java/swan/lake/activity/SLMasterActivity.kt
+++ app/src/main/java/swan/lake/activity/SLMasterActivity.kt
@@ -1,19 +1,72 @@
 package swan.lake.activity
 
+import android.content.Intent
 import android.os.Bundle
-import com.github.ajalt.timberkt.Timber
-import swan.atom.core.AtomCoreApplicationImpl
+import com.joanzapata.iconify.IconDrawable
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import kotlinx.android.synthetic.main.swan_lake_master.*
 import swan.atom.core.base.AtomCoreBaseActivity
+import swan.atom.core.icon.AtomCoreIconifyIcons
 import swan.lake.R
 
+
 class SLMasterActivity : AtomCoreBaseActivity() {
+
+    private var drawer: Drawer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.swan_lake_master)
 
-        Timber.e {
-            "AtomCoreApplicationImpl.getContext()::${AtomCoreApplicationImpl.getContext()}"
-        }
+//        setSupportActionBar(toolbar)
+        toolbar?.title = "Drawer"
+
+        drawer =
+                DrawerBuilder(this)
+                        .withRootView(R.id.drawer_container)
+                        .withToolbar(toolbar)
+                        .withDisplayBelowStatusBar(false)
+                        .withActionBarDrawerToggleAnimated(true)
+                        .addDrawerItems(
+                                PrimaryDrawerItem()
+                                        .withIdentifier(1)
+                                        .withName("Home")
+                                        .withSelectable(false)
+                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_EARTH)),
+
+                                PrimaryDrawerItem()
+                                        .withIdentifier(11)
+                                        .withName("Koala")
+                                        .withDescription("Go Koala")
+                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_DIAMOND)),
+
+                                PrimaryDrawerItem()
+                                        .withIdentifier(12)
+                                        .withName("Setting")
+                                        .withSelectable(false)
+                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_SETTING)),
+
+                                PrimaryDrawerItem()
+                                        .withIdentifier(13)
+                                        .withName("About")
+                                        .withSelectable(false)
+                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_INFORMATION))
+                        )
+                        .withOnDrawerItemClickListener { view, position, drawerItem ->
+                            when (drawerItem.identifier) {
+                                11L -> {
+                                    startActivity(Intent("swan.biz.koala.activity.MzituMasterActivity.action"))
+                                    true
+                                }
+
+                                else -> false
+                            }
+                        }
+                        .withShowDrawerOnFirstLaunch(true)
+                        .withSavedInstance(savedInstanceState)
+                        .build()
     }
 }
