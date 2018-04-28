3fff4d4431bfe19003544a62d6d669a87428d0a5
stephen.guo <stephen.guo@qunar.com>
2018-04-28 18:09:12 +0800

    封装toolbar

--- app/src/main/java/swan/lake/activity/SLMasterActivity.kt
+++ app/src/main/java/swan/lake/activity/SLMasterActivity.kt
@@ -1,12 +1,7 @@
 package swan.lake.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
-import com.joanzapata.iconify.IconDrawable
-import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import swan.atom.core.Cirrus
 import swan.atom.core.base.AtomCoreBaseActivity
 import swan.atom.core.icon.AtomCoreIconifyIcons
 import swan.lake.R
@@ -14,7 +9,7 @@ import swan.lake.R
 
 class SLMasterActivity : AtomCoreBaseActivity() {
 
-    private var drawer: Drawer? = null
+//    private var drawer: Drawer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,51 +17,60 @@ class SLMasterActivity : AtomCoreBaseActivity() {
 
 //        setSupportActionBar(toolbar)
 
-        findViewById<Toolbar>(R.id.cirrusCoreToolbar).title = "cirrusCoreToolbar"
+        val cirrus: Cirrus? = findViewById<Cirrus>(R.id.cirrus)
+        cirrus?.let {
+            Cirrus.Companion.CirrusBuilder()
+                    .withCirrusTitle("TTTTTTTTTTTTTTTTilte")
+                    .withCirrusSubtitle("TfafsafsafasfasfdsaTTTTTTTTTTTTTTTilte")
+                    .withCirrusNavigationBackPressIcon()
+                    .withCirrusRightComponent(AtomCoreIconifyIcons.ATOM_CORE_SHARE.value("\nDown"), null)
+                    .withCirrusMenu(R.menu.swan_lake_menu, null)
+                    .build(it)
+        }
 
-        drawer =
-                DrawerBuilder(this)
-                        .withRootView(R.id.drawer_container)
-//                        .withToolbar(toolbar)
-                        .withDisplayBelowStatusBar(false)
-                        .withActionBarDrawerToggleAnimated(true)
-                        .addDrawerItems(
-                                PrimaryDrawerItem()
-                                        .withIdentifier(1)
-                                        .withName("Home")
-                                        .withSelectable(false)
-                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_EARTH)),
-
-                                PrimaryDrawerItem()
-                                        .withIdentifier(11)
-                                        .withName("Koala")
-                                        .withDescription("Go Koala")
-                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_DIAMOND)),
-
-                                PrimaryDrawerItem()
-                                        .withIdentifier(12)
-                                        .withName("Setting")
-                                        .withSelectable(false)
-                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_SETTING)),
-
-                                PrimaryDrawerItem()
-                                        .withIdentifier(13)
-                                        .withName("About")
-                                        .withSelectable(false)
-                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_INFORMATION))
-                        )
-                        .withOnDrawerItemClickListener { view, position, drawerItem ->
-                            when (drawerItem.identifier) {
-                                11L -> {
-                                    startActivity(Intent("swan.biz.koala.activity.MzituMasterActivity.action"))
-                                    true
-                                }
-
-                                else -> false
-                            }
-                        }
-                        .withShowDrawerOnFirstLaunch(true)
-                        .withSavedInstance(savedInstanceState)
-                        .build()
+//        drawer =
+//                DrawerBuilder(this)
+//                        .withRootView(R.id.drawer_container)
+////                        .withToolbar(toolbar)
+//                        .withDisplayBelowStatusBar(false)
+//                        .withActionBarDrawerToggleAnimated(true)
+//                        .addDrawerItems(
+//                                PrimaryDrawerItem()
+//                                        .withIdentifier(1)
+//                                        .withName("Home")
+//                                        .withSelectable(false)
+//                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_EARTH)),
+//
+//                                PrimaryDrawerItem()
+//                                        .withIdentifier(11)
+//                                        .withName("Koala")
+//                                        .withDescription("Go Koala")
+//                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_DIAMOND)),
+//
+//                                PrimaryDrawerItem()
+//                                        .withIdentifier(12)
+//                                        .withName("Setting")
+//                                        .withSelectable(false)
+//                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_SETTING)),
+//
+//                                PrimaryDrawerItem()
+//                                        .withIdentifier(13)
+//                                        .withName("About")
+//                                        .withSelectable(false)
+//                                        .withIcon(IconDrawable(applicationContext, AtomCoreIconifyIcons.ATOM_CORE_INFORMATION))
+//                        )
+//                        .withOnDrawerItemClickListener { view, position, drawerItem ->
+//                            when (drawerItem.identifier) {
+//                                11L -> {
+//                                    startActivity(Intent("swan.biz.koala.activity.MzituMasterActivity.action"))
+//                                    true
+//                                }
+//
+//                                else -> false
+//                            }
+//                        }
+//                        .withShowDrawerOnFirstLaunch(true)
+//                        .withSavedInstance(savedInstanceState)
+//                        .build()
     }
 }
